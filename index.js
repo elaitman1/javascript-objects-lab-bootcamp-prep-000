5515c211c2f73e6c4195f128c4badf100085c76c
elaitman1 <elaitman1@gmail.com>
2018-07-19 01:27:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+function deleteFromObjectByKey(object, key){
+  var object2 = Object.assign({},object);
+  delete object2[key];
+  return object2;
+}
